042c7e9a73c160138b0d69ef294da4af0e9f1e54
Crowdbotics <team@crowdbotics.com>
2020-10-20 13:16:56 +0000

    Update models

--- backend/homekjhkjh/migrations/0001_initial.py
+++ backend/homekjhkjh/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='HGFHJGJHG',
+            name="HGFHJGJHG",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('kjhkjhkjh', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("kjhkjhkjh", models.BigIntegerField()),
             ],
         ),
     ]
